e6d0f5e681d4cf88c0032615d24b833bc5f0b014
Yehor Smoliakov <yehors+gh@ukr.net>
2020-05-22 10:26:10 +0300

    Improvements

--- webdev/create-mysql-db/README.md
+++ webdev/create-mysql-db/README.md
@@ -3,8 +3,8 @@ Scripts create a new database and user with granted permissions in MySQL.
 How to use:
 
 1) Change @DATABASENAME and @USERPASSWORD in create_mysql_db.sql
-2) Change root password in create_db.sh by --password= argument 
-3) Run ./create_db.sh
+2) Change root password in create_db.sh by the --password= argument 
+3) Run ./create_db.sh using the root user
 
 Notice:
 
--- webdev/create-mysql-db/create_db.sh
+++ webdev/create-mysql-db/create_db.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
+  echo "This script must be run as root"
+  exit 1
 fi
 
-mysql -u root --password=ROOT_PASSWORD < create_mysql_db.sql
+mysql -u root --password=ROOT_PASSWORD <create_mysql_db.sql
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Something went wrong"
-    exit 1
+  echo "Something went wrong"
+  exit 1
 fi
 
 echo "All is OK, the database was created"
